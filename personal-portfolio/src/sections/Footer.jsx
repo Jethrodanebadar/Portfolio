a04b1e5c3426605a086a880a4b7a563fe7882b59
Jethrodanebadar <jetdbadar@gmail.com>
2025-08-18 23:19:56 -0400

    Clean the footer for now

--- personal-portfolio/src/sections/Footer.jsx
+++ personal-portfolio/src/sections/Footer.jsx
@@ -2,22 +2,14 @@ import AnimatedHeaderSection from "../components/AnimatedHeaderSection";
 import AnimatedTextLine from "../components/AnimatedTextLine";
 
 const Footer = () => {
-  const contactText = `Hello 
-  To
-  The 
-  World`;
   return (
-    <section id="contact" className="bg-black h-[50vh]">
+    <section id="contact" className="bg-black min-h-[50vh]">
       <AnimatedHeaderSection
         subTitle={"Let’s connect"}
         title={"Contact"}
         text={""}
         textColor={"text-white"}
         withScrollTrigger={true}
-      />
-      <AnimatedTextLine
-        text={contactText}
-        className={` flex flex-col items-center justify-center font-light uppercase value-text-responsive ${"text-white/75"}`}
       />
     </section>
   );
