11a429f0dff5e0cf788a6e23e3ef31c984f7bae7
Jethrodanebadar <jetdbadar@gmail.com>
2025-08-18 23:32:08 -0400

    Rename it back to Header.jsx. This commit is to bypass git not recognizing me changing header.jsx to Header.jsx

--- personal-portfolio/src/App.jsx
+++ personal-portfolio/src/App.jsx
@@ -1,6 +1,6 @@
 import ReactLenis from "lenis/react";
 import About from "./sections/About";
-import Header from "./sections/temp";
+import Header from "./sections/Header";
 import Hero from "./sections/Hero";
 import Skills from "./sections/Skills";
 import Work from "./sections/Work";
